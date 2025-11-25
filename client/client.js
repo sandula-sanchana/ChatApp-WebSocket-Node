797b63f8de8d3882696568baf3c89a254bd3301d
sandula-sanchana <sandulasanchana@gmail.com>
2025-11-25 13:06:33 +0530

    add simple cli multi instance cli messaging to the server

--- client/client.js
+++ client/client.js
@@ -1,14 +1,26 @@
 const WebSocket = require('ws');
+const line=require('readline')
 
 const socket = new WebSocket("ws://localhost:2004");
 
 socket.on('open', () => {
     console.log("connected to server");
-
-    // Optionally send a message
     socket.send("Hello server!");
 });
 
 socket.on('message', (msg) => {
     console.log("Server says:", msg.toString());
 });
+
+let rl=line.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+    prompt: 'Say something: '
+})
+rl.prompt();
+
+rl.on('line',function (output){
+    socket.send(output)
+    rl.prompt();
+})
+
