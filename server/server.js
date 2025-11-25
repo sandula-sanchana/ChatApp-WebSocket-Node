c7ea605024e79d10fa8131461ce5d5abdc57c720
sandula-sanchana <sandulasanchana@gmail.com>
2025-11-25 17:48:37 +0530

    add multi client chatting with message broadcasting

--- server/server.js
+++ server/server.js
@@ -3,15 +3,24 @@ const events = require("node:events");
 
 const server_socket=new WebSocket.Server({port:2004});
 
+const clients=[]
 
+function broadCaster(){
+
+}
 server_socket.on('connection',(socket)=>{
     console.log("Client connected");
-
     socket.send("Welcome to the server!");
-
+    clients.push(socket)
 
     socket.on('message',(msg)=>{
         console.log(`client says : ${msg}`)
+
+        clients.map((s)=>{
+            if (s !== socket) {
+                s.send(msg.toString())
+            }
+        })
     })
 })
 
